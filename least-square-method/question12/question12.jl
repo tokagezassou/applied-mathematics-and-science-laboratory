e6328254557086ea91993924cc862af9401fca81
ruitybalt38 <rui.amano.tfa@gmail.com>
2025-11-06 16:09:23 +0900

    kadai12

--- least-square-method/question12/question12.jl
+++ least-square-method/question12/question12.jl
@@ -36,6 +36,13 @@ function run_kalman_filter(vector_y)
     theta_hat_prev = 3.0 # theta_0の平均値
     V_prev = 2.0 # theta_0の分散
 
+    filter_theta_hat = zeros(size + 1)
+    filter_V = zeros(size + 1)
+    predict_X = zeros(size + 1)
+
+    filter_theta_hat[1] = theta_hat_prev
+    filter_V[1] = V_prev
+
     for k in 1:size
         y_k = vector_y[k]
         
@@ -45,23 +52,60 @@ function run_kalman_filter(vector_y)
         V_k = (1.0 - F_k * c) * X_k
         
         vector_theta_hat[k] = theta_hat_k
+
+        predict_X[k+1] = X_k
+        filter_theta_hat[k+1] = theta_hat_k
+        filter_V[k+1] = V_k
         
         theta_hat_prev = theta_hat_k
         V_prev = V_k
     end
     
-    return vector_theta_hat
+    return filter_theta_hat, filter_V, predict_X
+end
+
+function run_kalman_smoother(filter_theta_hat, filter_V, predict_X)
+    size = 100
+    a = 0.9
+    
+    smooth_theta_hat = zeros(size + 1)
+    smooth_V = zeros(size + 1)
+
+    smooth_theta_hat[size+1] = filter_theta_hat[size+1]
+    smooth_V[size+1] = filter_V[size+1]
+
+    for k in (size):-1:1
+        V_k = filter_V[k]
+        X_k_plus_1 = predict_X[k+1]
+        theta_hat_k = filter_theta_hat[k]
+
+        theta_hat_k_plus_1_s = smooth_theta_hat[k+1]
+        V_k_plus_1_s = smooth_V[k+1]
+
+        g_k = a * V_k / X_k_plus_1
+        
+        smooth_theta_hat[k] = theta_hat_k + g_k * (theta_hat_k_plus_1_s - a * theta_hat_k)
+        smooth_V[k] = V_k + g_k^2 * (V_k_plus_1_s - X_k_plus_1)
+    end
+    
+    return smooth_theta_hat, smooth_V
 end
 
 function main()
     open("output.dat", "w") do f
         vector_theta_true, vector_y = generate_data()
-        vector_theta_hat = run_kalman_filter(vector_y)
+        filter_theta_hat, filter_V, predict_X = run_kalman_filter(vector_y)
+        smooth_theta_hat, smooth_V = run_kalman_smoother(filter_theta_hat, filter_V, predict_X)
+
+        theta_0_hat_smooth = smooth_theta_hat[1]
+        V_0_smooth = smooth_V[1]
+        V_0_initial = filter_V[1]
+        ratio = V_0_smooth / V_0_initial
         
-        println(f, "# k  theta_hat  theta_true")
-        for k in 1:100
-            println(f, "$k  $(vector_theta_hat[k])  $(vector_theta_true[k])")
-        end
+        println(f, "theta_0_hat_smooth = $theta_0_hat_smooth")
+        println(f, "V_0_smooth = $V_0_smooth")
+        println(f, "V_0_initial = $V_0_initial")
+        println(f, "ratio = $ratio")
     end
 end
 
