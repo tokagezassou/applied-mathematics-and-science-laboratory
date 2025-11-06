36702eb80af14626e3c1eff8d8b2424243b56cc0
ruitybalt38 <rui.amano.tfa@gmail.com>
2025-11-06 11:24:28 +0900

    課題９でいろいろ試したい

--- least-square-method/question9/question9.jl
+++ least-square-method/question9/question9.jl
@@ -4,33 +4,26 @@ using LinearAlgebra
 using Statistics
 
 # 課題9.1
-function estimate_parameters_with_constant_force() 
+function estimate_parameters_with_constant_force(dt) 
     M_true = 2.0
     D_true = 1.0
     K_true = 3.0
-    dt = 0.01
     epsilon = 1.0e-6
 
-    theta1_true = 2.0 - (D_true / M_true) * dt
-    theta2_true = -(1.0 - (D_true / M_true) * dt + (K_true / M_true) * dt^2)
-    theta3_true = (dt^2) / M_true
+    theta_true = from_MDK_to_theta(M_true, D_true, K_true, dt)
      
     theta_hat = zeros(3)
-    Phi_tilde = (1.0 / epsilon) * Matrix(I, 3, 3) 
+    Phi_tilde = (1.0 / epsilon) * Matrix(I, 3, 3)
 
-    y_km2 = 0.0 
-    y_km1 = 0.0 
+    y_km2 = 0.0
+    y_km1 = 0.0
     
-    F_km2 = 1.0 
-    F_km1 = 1.0
-    F_k = 1.0         
-
-    theta_history = zeros(10000, 3)
+    F_km2 = 1.0
 
     for k in 1:10000
         w_k = rand() * 2.0 - 1.0 
         
-        y_k = theta1_true * y_km1 + theta2_true * y_km2 + theta3_true * F_km2 + w_k
+        y_k = theta_true[1] * y_km1 + theta_true[2] * y_km2 + theta_true[3] * F_km2 + w_k
 
         phi_k = [y_km1 y_km2 F_km2] 
         phi_k_T = transpose(phi_k)
@@ -42,30 +35,121 @@ function estimate_parameters_with_constant_force()
         theta_hat = theta_hat + K_k * prediction_error
         Phi_tilde = Phi_tilde - K_k * phi_k * Phi_tilde
 
-        theta_history[k, :] = theta_hat
-
         y_km2 = y_km1
-        y_km1 = ymk 
-        F_km2 = F_km1
-        F_km1 = F_k
+        y_km1 = y_k
     end
 
     return theta_hat
 end
 
-function from_theta_hat_to_MDK(theta_hat)
-    M = (DT^2) / theta_hat[3]
-    D = (2.0 - theta_hat[1]) * M_est / DT
-    K = (1.0 - theta_hat[1] - theta_hat[2]) * M_est / (DT^2)
+# 課題9.2
+function estimate_parameters_with_sin_force(dt) 
+    M_true = 2.0
+    D_true = 1.0
+    K_true = 3.0
+    epsilon = 1.0e-6
 
+    theta_true = from_MDK_to_theta(M_true, D_true, K_true, dt)
+     
+    theta_hat = zeros(3)
+    Phi_tilde = (1.0 / epsilon) * Matrix(I, 3, 3)
+
+    y_km2 = 0.0
+    y_km1 = 0.0
+
+    for k in 1:10000
+        w_k = rand() * 2.0 - 1.0
+        F_km2 = sin((k-2) * pi / 5)
+        
+        y_k = theta_true[1] * y_km1 + theta_true[2] * y_km2 + theta_true[3] * F_km2 + w_k
+
+        phi_k = [y_km1 y_km2 F_km2] 
+        phi_k_T = transpose(phi_k)
+        
+        denominator = 1.0 + (phi_k * Phi_tilde * phi_k_T)[1]
+        K_k = (Phi_tilde * phi_k_T) / denominator
+ 
+        prediction_error = y_k - (phi_k * theta_hat)[1]
+        theta_hat = theta_hat + K_k * prediction_error
+        Phi_tilde = Phi_tilde - K_k * phi_k * Phi_tilde
+
+        y_km2 = y_km1
+        y_km1 = y_k
+    end
+
+    return theta_hat
+end
+
+# 課題9.3
+function estimate_parameters_with_custom_force(dt) 
+    M_true = 2.0
+    D_true = 1.0
+    K_true = 3.0
+    epsilon = 1.0e-6
+
+    theta_true = from_MDK_to_theta(M_true, D_true, K_true, dt)
+     
+    theta_hat = zeros(3)
+    Phi_tilde = (1.0 / epsilon) * Matrix(I, 3, 3)
+
+    y_km2 = 0.0
+    y_km1 = 0.0
+
+    for k in 1:10000
+        w_k = rand() * 2.0 - 1.0
+        F_km2 = 10 * sin(13 * (k-2) * pi / 11)
+        
+        y_k = theta_true[1] * y_km1 + theta_true[2] * y_km2 + theta_true[3] * F_km2 + w_k
+
+        phi_k = [y_km1 y_km2 F_km2] 
+        phi_k_T = transpose(phi_k)
+        
+        denominator = 1.0 + (phi_k * Phi_tilde * phi_k_T)[1]
+        K_k = (Phi_tilde * phi_k_T) / denominator
+ 
+        prediction_error = y_k - (phi_k * theta_hat)[1]
+        theta_hat = theta_hat + K_k * prediction_error
+        Phi_tilde = Phi_tilde - K_k * phi_k * Phi_tilde
+
+        y_km2 = y_km1
+        y_km1 = y_k
+    end
+
+    return theta_hat
+end
+
+function from_MDK_to_theta(M, D, K, dt)
+    theta1 = 2.0 - (D / M) * dt
+    theta2 = -(1.0 - (D / M) * dt + (K / M) * dt^2)
+    theta3 = (dt^2) / M
+    return [theta1, theta2, theta3]
+end
+
+function from_theta_to_MDK(theta, dt)
+    M = (dt^2) / theta[3]
+    D = (2.0 - theta[1]) * M / dt
+    K = (1.0 - theta[1] - theta[2]) * M / (dt^2)
     return M, D, K
 end
 
 function main()
     open("output.dat", "w") do f
-        theta_hat = estimate_parameters_with_constant_force()
-        M_est, D_est, K_est = from_theta_hat_to_MDK(theta_hat)
-        print(f, M_est, D_est,K_est)
+        dt = 0.01
+
+        println(f, "----- constant_force -----")
+        theta_hat = estimate_parameters_with_constant_force(dt)
+        M_est, D_est, K_est = from_theta_to_MDK(theta_hat, dt)
+        println(f, "$M_est  $D_est  $K_est")
+
+        println(f, "----- sin_force -----")
+        theta_hat = estimate_parameters_with_sin_force(dt)
+        M_est, D_est, K_est = from_theta_to_MDK(theta_hat, dt)
+        println(f, "$M_est  $D_est  $K_est")
+
+        println(f, "----- custom_force -----")
+        theta_hat = estimate_parameters_with_custom_force(dt)
+        M_est, D_est, K_est = from_theta_to_MDK(theta_hat, dt)
+        println(f, "$M_est  $D_est  $K_est")
     end
 end
 
