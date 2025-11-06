2282f2dec14e4e5f2d3f7950ebf72750ae73b489
ruitybalt38 <rui.amano.tfa@gmail.com>
2025-11-06 11:56:53 +0900

    課題９まで

--- least-square-method/question9/question9.jl
+++ least-square-method/question9/question9.jl
@@ -81,7 +81,7 @@ function estimate_parameters_with_sin_force(dt)
 end
 
 # 課題9.3
-function estimate_parameters_with_custom_force(dt) 
+function estimate_parameters_with_huge_force(dt) 
     M_true = 2.0
     D_true = 1.0
     K_true = 3.0
@@ -97,11 +97,49 @@ function estimate_parameters_with_custom_force(dt)
 
     for k in 1:10000
         w_k = rand() * 2.0 - 1.0
-        F_km2 = 10 * sin(13 * (k-2) * pi / 11)
+        F_km2 = 1.0e6
         
         y_k = theta_true[1] * y_km1 + theta_true[2] * y_km2 + theta_true[3] * F_km2 + w_k
 
-        phi_k = [y_km1 y_km2 F_km2] 
+        phi_k = [y_km1 y_km2 F_km2]
+        phi_k_T = transpose(phi_k)
+        
+        denominator = 1.0 + (phi_k * Phi_tilde * phi_k_T)[1]
+        K_k = (Phi_tilde * phi_k_T) / denominator
+ 
+        prediction_error = y_k - (phi_k * theta_hat)[1]
+        theta_hat = theta_hat + K_k * prediction_error
+        Phi_tilde = Phi_tilde - K_k * phi_k * Phi_tilde
+
+        y_km2 = y_km1
+        y_km1 = y_k
+    end
+
+    return theta_hat
+end
+
+# 課題9.3
+function estimate_parameters_with_random_force(dt) 
+    M_true = 2.0
+    D_true = 1.0
+    K_true = 3.0
+    epsilon = 1.0e-6
+
+    theta_true = from_MDK_to_theta(M_true, D_true, K_true, dt)
+     
+    theta_hat = zeros(3)
+    Phi_tilde = (1.0 / epsilon) * Matrix(I, 3, 3)
+
+    y_km2 = 0.0
+    y_km1 = 0.0
+
+    for k in 1:10000
+        w_k = rand() * 2.0 - 1.0
+        F_km2 = 1.0e3 * sin((k-2) *  1.7)
+        
+        y_k = theta_true[1] * y_km1 + theta_true[2] * y_km2 + theta_true[3] * F_km2 + w_k
+
+        phi_k = [y_km1 y_km2 F_km2]
         phi_k_T = transpose(phi_k)
         
         denominator = 1.0 + (phi_k * Phi_tilde * phi_k_T)[1]
@@ -146,8 +184,13 @@ function main()
         M_est, D_est, K_est = from_theta_to_MDK(theta_hat, dt)
         println(f, "$M_est  $D_est  $K_est")
 
-        println(f, "----- custom_force -----")
-        theta_hat = estimate_parameters_with_custom_force(dt)
+        println(f, "----- huge_force -----")
+        theta_hat = estimate_parameters_with_huge_force(dt)
+        M_est, D_est, K_est = from_theta_to_MDK(theta_hat, dt)
+        println(f, "$M_est  $D_est  $K_est")
+
+        println(f, "----- random_force -----")
+        theta_hat = estimate_parameters_with_random_force(dt)
         M_est, D_est, K_est = from_theta_to_MDK(theta_hat, dt)
         println(f, "$M_est  $D_est  $K_est")
     end
